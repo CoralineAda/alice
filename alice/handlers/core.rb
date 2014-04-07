148c3c6e1ba0ff64de3084a3b7d809f40f5af77f
Bantik <corey@idolhands.com>
2014-04-06 20:21:19 -0500

    Tweakages

--- alice/handlers/core.rb
+++ alice/handlers/core.rb
@@ -10,7 +10,7 @@ module Alice
 
       match /[hi|hello|heya|hey] alicebot/i, method: :greet, use_prefix: false
       match /\!cookie (.+)/, method: :cookie, use_prefix: false
-      match /\!pants.+?/, method: :pants, use_prefix: false
+      match /\!pants/, method: :pants, use_prefix: false
       match /\!help/, method: :help, use_prefix: false
 
       def greet(m)
