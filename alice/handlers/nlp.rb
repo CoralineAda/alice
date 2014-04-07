f17408d8f363cf5aa0e168d11ec35bd303bb6679
Bantik <corey@idolhands.com>
2014-04-07 09:59:48 -0500

    Better alice detection

--- alice/handlers/nlp.rb
+++ alice/handlers/nlp.rb
@@ -10,8 +10,7 @@ module Alice
 
       match /(.?[ ]?alice.?)/i, method: :process, use_prefix: false
 
-      def process(m, string_one, string_two="")
-        message = "#{string_one} #{string_2}"
+      def process(m, message)
         response = Alice::Command.parse(message) 
         response && response.kind == :reply && m.reply(response.content)
         response && response.kind == :action && m.action_reply(response.content)
