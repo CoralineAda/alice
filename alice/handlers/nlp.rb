c1d49a47877425c30815b6e62eb97c81f85a9a49
Bantik <corey@idolhands.com>
2014-04-07 09:36:38 -0500

    Wiring up NLP processor

---
+++ alice/handlers/nlp.rb
@@ -0,0 +1,24 @@
+require 'cinch'
+
+module Alice
+
+  module Handlers
+
+    class Nlp
+
+      include Cinch::Plugin
+
+      match /(.+)[ ]?alice(.+)/i, method: :process, use_prefix: false
+  
+      def process(m, string_one, string_two)
+        message = "#{string_one} #{string_2}"
+        response = Alice::Command.parse(message) 
+        response && response.kind == :reply && m.reply(response.content)
+        response && response.kind == :action && m.action_reply(response.content)
+      end    
+
+    end
+
+  end
+
+end
