261f62cb40bee65ec9e0c2f8826353bc5f268b82
Bantik <corey@idolhands.com>
2014-04-08 11:31:35 -0500

    Let treasure giver deal with beverages.

--- alice/handlers/treasure_giver.rb
+++ alice/handlers/treasure_giver.rb
@@ -12,12 +12,17 @@ module Alice
         return unless command.present?
         grams = Alice::Parser::NgramFactory.new(command.gsub(/[^a-zA-Z0-9\_\ ]/, '')).omnigrams - [Alice.bot.bot.nick]
         nick = grams.map{|g| Alice.bot.exists?(g.join(' ')) && g}.compact.flatten.last
+        return unless nick
         user = Alice::User.find_or_create(nick)
         treasure = Alice::Treasure.from(command).last
-        return unless nick
-        return unless treasure
-        treasure.to(nick)
-        Alice::Response.new(content: treasure.message, kind: :reply)
+        beverage = Alice::Beverage.from(command).last
+        if treasure
+          treasure.to(nick)
+          Alice::Response.new(content: treasure.message, kind: :reply)
+        elsif beverage
+          beverage.pass_to(nick)
+          Alice::Response.new(content: beverage.message, kind: :reply)
+        end
       end
 
     end
