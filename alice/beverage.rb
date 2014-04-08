1d53d4d78ae185989583f15a2b043d27cfdc3ebf
Bantik <corey@idolhands.com>
2014-04-08 11:36:08 -0500

    beverage passing bug

--- alice/beverage.rb
+++ alice/beverage.rb
@@ -47,9 +47,8 @@ class Alice::Beverage
     if recipient = Alice::User.find_or_create(owner)
       if recipient.is_bot?
         self.message = "#{recipient.primary_nick} does not accept drinks."
-      end
-      if transferable?
-        self.message = "You pass the #{self.name} to #{recipient.primary_nick.capitalize}."
+      else
+        self.message = "#{owner} passes the #{self.name} to #{recipient.primary_nick.capitalize}. Cheers!"
         self.message = self.message.gsub("the the", "the").gsub("the ye", "ye")
         self.user = recipient
         self.save
