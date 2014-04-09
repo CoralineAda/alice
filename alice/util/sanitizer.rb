03c34046ec7fd8a239e4ffd5d20d054841c21c23
Bantik <corey@idolhands.com>
2014-04-09 18:57:41 -0500

    Sanitizer fix

--- alice/util/sanitizer.rb
+++ alice/util/sanitizer.rb
@@ -5,11 +5,11 @@ module Alice
     class Sanitizer
 
       def self.process(text)
-        text.gsub!("the the", "the")
-        text.gsub!("the ye", "ye")
-        text.gsub!("a the", "a")
-        text.gsub!("a ye", "ye")
-        text.gsub!("ye ye", "ye")
+        text.gsub!("the the ", "the ")
+        text.gsub!("the ye ", "ye ")
+        text.gsub!("a the ", "a ")
+        text.gsub!("a ye ", "ye ")
+        text.gsub!("ye ye ", "ye ")
         text.gsub!("a a", "an a")
         text.gsub!("a e", "an e")
         text.gsub!("a i", "an i")
