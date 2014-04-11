8a687d981bc9ed5d74f50132a31d435894d194de
Bantik <corey@idolhands.com>
2014-04-11 10:40:13 -0500

    Seasrchable genius fix #hellyeah

--- alice/behavior/searchable.rb
+++ alice/behavior/searchable.rb
@@ -13,13 +13,20 @@ module Alice
         def from(string)
           return [] unless string.present?
           names = Alice::Parser::NgramFactory.new(string.gsub(/[^a-zA-Z0-9\-\_\ ]/, '')).omnigrams.map{|g| g.join ' '} << string
-          names.map{|name| like(name) }.flatten.compact.uniq || []
+          objects = names.map{|name| SearchResult.new(term: name, result: like(name).first) }
+          objects = objects.select{|obj| obj.result.present?}.uniq || []
+          objects.sort{|a,b| a.term.length <=> b.term.length}.map(&:result)
         end
 
         def like(name)
           where(name: /#{Regexp.escape(name)}$/i)
         end
         
+      end
+
+      class SearchResult
+        include PoroPlus
+        attr_accessor :term, :result
       end
 
     end
