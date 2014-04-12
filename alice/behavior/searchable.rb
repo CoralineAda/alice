407885e90d8c7f2ba8c004b69f7ba51d02a332b0
Bantik <corey@idolhands.com>
2014-04-11 19:31:02 -0500

    Search fix again

--- alice/behavior/searchable.rb
+++ alice/behavior/searchable.rb
@@ -19,7 +19,7 @@ module Alice
             end
           end.compact
           objects = objects.select{|obj| obj.result.present?}.uniq || []
-          objects.sort{|b,a| b.term.length <=> a.term.length}.map(&:result)
+          objects.sort{|b,a| b.term.length <=> a.term.length}.map(&:result).last
         end
 
         def like(name)
