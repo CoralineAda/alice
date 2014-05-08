24a036801007ea1b4cdf928d318a7269bb4996af
Bantik <corey@idolhands.com>
2014-05-08 18:41:21 -0500

    Minor cleanup in nlp

--- alice/listeners/nlp.rb
+++ alice/listeners/nlp.rb
@@ -13,10 +13,10 @@ module Alice
       match /(.*[ ]?alice.*)/i, method: :process, use_prefix: false
 
       def process(channel_user, message)
-        response = Alice::Command.parse(channel_user.user.nick, message) 
-        response && response.kind == :reply && Alice::Util::Mediator.reply_to(channel_user, response.content)
-        response && response.kind == :action && Alice::Util::Mediator.emote_to(channel_user, response.content)
-      end    
+        return unless response = Alice::Command.parse(channel_user.user.nick, message)
+        response.kind == :reply && Alice::Util::Mediator.reply_to(channel_user, response.content)
+        response.kind == :action && Alice::Util::Mediator.emote_to(channel_user, response.content)
+      end
 
     end
 
