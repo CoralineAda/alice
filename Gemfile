dda28bd2fd01aeee9ad13e3a6520ab6a7992d221
Bantik <corey@idolhands.com>
2014-05-08 18:45:21 -0500

    Partial merge

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem "cinch"
 gem "mongoid", "~> 3.1.6"
 gem "dotenv"
-gem 'ruby-stemmer', '>=0.8.3', :require => 'lingua/stemmer'
+gem 'ruby-stemmer', '>=0.8.3', require: 'lingua/stemmer'
 gem 'poro_plus'
-gem 'rspec'
+gem 'marky_markov'
+gem 'require_all'
--- alice.rb
+++ alice.rb
@@ -3,6 +3,7 @@ require 'bundler'
 require 'bundler/setup'
 require 'cinch'
 require 'dotenv'
+require 'require_all'
 
 Dotenv.load
 Bundler.require
@@ -10,57 +11,7 @@ Mongoid.load!("config/mongoid.yml")
 
 module Alice
 
-  require_relative 'alice/behavior/emotes'
-  require_relative 'alice/behavior/has_inventory'
-  require_relative 'alice/behavior/ownable'
-  require_relative 'alice/behavior/placeable'
-  require_relative 'alice/behavior/scorable'
-  require_relative 'alice/behavior/searchable'
-  require_relative 'alice/behavior/steals'
-
-  require_relative 'alice/filters/dazed'
-  require_relative 'alice/filters/drunk'
-  require_relative 'alice/filters/disoriented'
-
-  require_relative 'alice/handlers/beverage'
-  # require_relative 'alice/handlers/bio'
-  # require_relative 'alice/handlers/factoid'
-  # require_relative 'alice/handlers/greeting'
-  # require_relative 'alice/handlers/inventory'
-  # require_relative 'alice/handlers/oh'
-  # require_relative 'alice/handlers/response'
-  # require_relative 'alice/handlers/item_finder'
-  # require_relative 'alice/handlers/item_giver'
-  # require_relative 'alice/handlers/item_lister'
-  # require_relative 'alice/handlers/twitter'
-
-  require_relative 'alice/parser/language_helper'
-  require_relative 'alice/parser/ngram'
-  require_relative 'alice/parser/ngram_factory'
-
-  require_relative 'alice/user'
-  require_relative 'alice/action'
-  require_relative 'alice/actor'
-  require_relative 'alice/beverage'
-  require_relative 'alice/bio'
-  require_relative 'alice/bot'
-  require_relative 'alice/catchphrase'
-  require_relative 'alice/direct_command'
-  require_relative 'alice/fuzzy_command'
-  require_relative 'alice/command_string'
-  require_relative 'alice/dungeon'
-  require_relative 'alice/factoid'
-  require_relative 'alice/item'
-  require_relative 'alice/listener'
-  require_relative 'alice/locator'
-  require_relative 'alice/machine'
-  require_relative 'alice/oh'
-  require_relative 'alice/leaderboard'
-  require_relative 'alice/place'
-
-  require_relative 'alice/util/mediator'
-  require_relative 'alice/util/randomizer'
-  require_relative 'alice/util/sanitizer'
+  require_all 'alice'
 
   def self.start
     @@bot = Alice::Bot.new
