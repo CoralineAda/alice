fc0491b2de7740759b4222f24c61f6f274651107
Bantik <corey@idolhands.com>
2014-06-07 23:33:12 -0500

    Move bio formatting; user specs.

--- alice/bio.rb
+++ alice/bio.rb
@@ -15,8 +15,11 @@ class Alice::Bio
   end
 
   def formatted
-    return unless self.user.present?
-    "#{self.user.proper_name} is #{self.text}"
+    return "It's #{self.user.proper_name}! " unless self.user.present?
+    formatted_text = self.text.gsub(/^is /, ' ')
+    formatted_text = "#{self.user.proper_name} is #{formatted_text}"
+    formatted_text = formatted_text.gsub(/is (was|has|had|does|doesn.t) /, '\1 ')
+    formatted_text = formatted_text.gsub(/[ ]+/, " ")
   end
 
 end
