90f14cb8151e7e36ff8520eca4c1e2b60b289515
Bantik <corey@idolhands.com>
2014-06-22 23:01:01 -0500

    Move logic out of handlers, yay!

--- alice/constants.rb
+++ alice/constants.rb
@@ -1,5 +1,6 @@
 module Constants
 
+  NO_SUCH_DRINK = "You've got the straw but not the cup, if you know what I mean."
   THATS_ENOUGH_DONTCHA_THINK = "Free yourself from the material world. Or get rid of some stuff before you make more. Your choice."
   THERE_CAN_BE_ONLY_ONE = "Sorry, but our license with SingletonCorp LLC clearly states that there can only be one of those in existence at a time."
   UH_OH = "Something went terribly wrong!"
