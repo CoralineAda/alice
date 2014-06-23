da96e1ee5d84738775cb3e005f05ed1846224636
Bantik <corey@idolhands.com>
2014-06-22 23:45:49 -0500

    Sample fixes; work on factoids

--- alice/handlers/factoid.rb
+++ alice/handlers/factoid.rb
@@ -1,34 +1,13 @@
-module Alice
+module Handlers
 
-  module Handlers
+  class Factoid
 
-    class Factoid
+    include PoroPlus
+    include Behavior::HandlesCommands
 
-      def self.minimum_indicators
-        3
-      end
-
-      def self.process(sender, command)
-        if subject = User.from(command)
-          if factoid = subject.factoids.sample
-            Alice::Handlers::Response.new(content: factoid.formatted, kind: :reply)
-          end
-        elsif command =~ /about (.+)$/
-          subject = command.split('about')[-1].strip
-          if response = Alice::Factoid.about(subject)
-            Alice::Handlers::Response.new(content: response.formatted, kind: :reply)
-          else
-            Alice::Handlers::Response.new(content: Alice::Util::Randomizer.negative_response, kind: :reply)
-          end
-        else
-          Alice::Handlers::Response.new(content: random.formatted, kind: :reply)
-        end
-      end
-
-      def self.random
-        Alice::Factoid.random
-      end
-
+    def process
+      message.response = ::Factoid.about(message.trigger).formatted
+      message
     end
 
   end
