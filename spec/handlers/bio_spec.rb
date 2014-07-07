7cecc5cb653b27b09d42643f28b74c09b04e8571
Bantik <corey@idolhands.com>
2014-07-07 05:21:25 -0500

    Factoid, bio handler specs

--- spec/handlers/bio_spec.rb
+++ spec/handlers/bio_spec.rb
@@ -2,31 +2,32 @@ require 'spec_helper'
 
 describe "Handlers::Bio" do
 
-  let(:user)    { User.new(primary_nick: "Aaron") }
-  let(:message) { Message.new(user.primary_nick, "drink coffee") }
-  let(:handler) { Handlers::Beverage.new(message: message) }
+  let(:user)    { User.new(primary_nick: "Sindarina") }
+  let(:message) { Message.new(user.primary_nick, "is a star") }
+  let(:handler) { Handlers::Bio.new(message: message) }
 
   before do
    message.stub(:sender) { user }
+   handler.stub(:subject) { user }
   end
 
-  describe "#brew" do
+  describe "#set" do
     it "is wired to a working method" do
-      expect(::Beverage.respond_to?(:brew)).to be_true
+      expect(user.respond_to?(:update_bio)).to be_true
     end
-    it "calls Beverage.brew" do
-      expect(::Beverage).to receive(:brew)
-      handler.brew
+    it "calls update_bio" do
+      expect(user).to receive(:update_bio)
+      handler.set
     end
   end
 
-  describe "#drink" do
+  describe "#get" do
     it "is wired to a working method" do
-      expect(::Beverage.respond_to?(:consume)).to be_true
+      expect(user.respond_to?(:formatted_bio)).to be_true
     end
-    it "calls Beverage.brew" do
-      expect(::Beverage).to receive(:consume)
-      handler.drink
+    it "calls formatted_bio" do
+      expect(user).to receive(:formatted_bio)
+      handler.get
     end
   end
 
---
+++ spec/handlers/factoid_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Handlers::Factoid" do
+
+  let(:user)    { User.new(primary_nick: "zspenser") }
+  let(:message) { Message.new(user.primary_nick, "once found a bug") }
+  let(:handler) { Handlers::Factoid.new(message: message) }
+
+  before do
+   message.stub(:sender) { user }
+   handler.stub(:subject) { user }
+  end
+
+  describe "#set" do
+    it "is wired to a working method" do
+      expect(user.respond_to?(:set_factoid)).to be_true
+    end
+    it "calls the correct method" do
+      expect(user).to receive(:set_factoid)
+      handler.set
+    end
+  end
+
+  describe "#get" do
+    it "is wired to a working method" do
+      expect(user.respond_to?(:random_factoid)).to be_true
+    end
+    it "calls the correct method" do
+      expect(user).to receive(:random_factoid) { ::Factoid.new }
+      handler.get
+    end
+  end
+
+end
