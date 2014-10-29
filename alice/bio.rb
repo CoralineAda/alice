c03359c8527bd3767ab2e9cec220aa7ad5e98bfe
Bantik <corey@idolhands.com>
2014-10-29 14:31:00 -0500

    Fix a spacing bug in bio.

--- alice/bio.rb
+++ alice/bio.rb
@@ -34,7 +34,7 @@ class Bio
     return Constants::WHO_DAT unless self.user
     return "It's #{self.user.proper_name}! " unless self.user.present?
     formatted_text = self.text.gsub(/^is /, ' ')
-    formatted_text = "#{self.user.proper_name} is #{formatted_text}"
+    formatted_text = "#{self.user.proper_name} is #{formatted_text}. "
     formatted_text = formatted_text.gsub(/is (was|has|had|does|doesn.t) /, '\1 ')
     formatted_text = formatted_text.gsub(/[ ]+/, " ")
   end
