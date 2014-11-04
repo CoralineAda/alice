497c1c624557368b3642ef1241dd53779a530d02
Bantik <corey@idolhands.com>
2014-11-03 21:35:09 -0600

    Fix frequencies

--- alice/filters/disoriented.rb
+++ alice/filters/disoriented.rb
@@ -6,7 +6,7 @@ module Alice
 
       def process(text)
         to_process = "#{text}"
-        directions = (Place::DIRECTIONS + ["up", "down", "sideways", "diagonally"])
+        directions = (Place::DIRECTIONS + ["up", "down", "sideways", "dennis", "diagonally"])
         directions = directions.sort{|a,b| rand(10) <=> rand(10)}
         to_process.gsub!(/east|west|north|south/) { directions.pop }
       end
