bcb0f6a47b34f81d324681f90c277c93068fb7a5
Bantik <corey@idolhands.com>
2014-11-05 13:57:25 -0600

    Stop cleaning search strings

--- alice/behavior/searchable.rb
+++ alice/behavior/searchable.rb
@@ -17,9 +17,9 @@ module Alice
         # Use when there are multiple words to parse, e.g. pulling a name from a string.
         def from(string)
           return unless string.present?
-          clean_string = string.gsub(/[^a-zA-Z0-9\-\_\ ]/, '')
-          clean_string.gsub!(/'s/, '')
-          names = Alice::Parser::NgramFactory.new(clean_string).omnigrams
+          # # clean_string = string.gsub(/[^a-zA-Z0-9\-\_\ ]/, '')
+          # clean_string.gsub!(/'s/, '')
+          names = Alice::Parser::NgramFactory.new(string).omnigrams
           names = names.map{|g| g.join ' '} << string
           names.uniq.reject!{|name| Alice::Parser::LanguageHelper::IDENTIFIERS.include?(name)}
           objects = names.map do |name|
