f4e8b2ef36a3c3d83b896c8dc76d1cf8e00bda19
CoralineAda <coraline@idolhands.com>
2014-11-28 12:28:09 -0600

    Strip whitespace from URL previews

--- alice/processor.rb
+++ alice/processor.rb
@@ -70,11 +70,16 @@ class Processor
   def preview_url
     source = Nokogiri::HTML(open(trigger))
     title_node = source.search("//title")
-    snippet = source.search("//p").first.content[0..254]
+    source.search("//p").map(&:content).each do |content|
+      next if content.length < 25
+      snippet ||= ""; snippet << content[0..254]
+      break if snippet.length > 255
+    end
+    snippet = snippet.strip.gsub(/[\n\r ]+/," ")[0..254]
     preview = "#{title_node && title_node.text}... #{snippet}..."
     Alice::Util::Mediator.reply_with(
       self.channel,
-      Response.url_preview(self.message, preview.strip.gsub(/[\n\r]+/," ")).response
+      Response.url_preview(self.message, ).response
     )
   rescue Exception => e
     Alice::Util::Logger.info("*** Couldn't process URL preview for #{trigger}: #{e}")
