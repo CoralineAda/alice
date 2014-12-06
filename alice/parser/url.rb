b117b3cc9032b71532a15ef4ff67a8d0337672a6
Coraline <coraline@idolhands.com>
2014-12-06 04:39:45 +0000

    Clean up URL preview

--- alice/parser/url.rb
+++ alice/parser/url.rb
@@ -6,30 +6,38 @@ module Alice
   module Parser
 
     class URL
-      
+
       attr_reader :url
-      
+
       def initialize(url)
         @url = url
       end
-      
+
       def preview
         source = Nokogiri::HTML(open(url))
         title_node = source.search("//title")
-        source.search("//p").map(&:content).each do |content|
-          next if content.length < 25
-          snippet ||= ""; snippet << content[0..254]
-          break if snippet.length > 255
+        snippet = source.xpath("//p").map(&:content).detect do |content|
+          content.length > 25
         end
-        snippet = snippet.to_s.strip.gsub(/[\n\r ]+/," ")[0..254]
-        title = title_node.nil? ? '' : title_node.text
-        return [title, snippet].reject(&:empty?).join('| ')
+        snippet = truncate(snippet.to_s.strip.gsub(/[\n\r ]+/," "))
+        title   = truncate(title_node.nil? ? '' : title_node.text)
+        return [title, snippet].reject(&:empty?).join(' | ')
       rescue Exception => e
         Alice::Util::Logger.info("*** Couldn't process URL preview for #{url}: #{e.backtrace}")
       end
-      
+
+      private
+
+      def truncate(text, max_length = 255)
+        if text.length > max_length
+          "#{text[0..max_length].gsub(/\s+$/, '')}..."
+        else
+          text
+        end
+      end
+
     end
-    
+
   end
-  
+
 end
