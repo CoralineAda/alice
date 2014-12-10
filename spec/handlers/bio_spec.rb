8812137bb562310f1f0b3d9cd8509000bac82b4b
CoralineAda <coraline@idolhands.com>
2014-12-09 22:58:34 -0600

    Spec tweak

--- spec/handlers/bio_spec.rb
+++ spec/handlers/bio_spec.rb
@@ -6,8 +6,10 @@ describe "Handlers::Bio" do
   let(:user_2)    { User.create(primary_nick: "Wuest") }
   let(:message_1) { Message::Message.new(user_1.primary_nick, "\"is a star\"") }
   let(:message_2) { Message::Message.new(user_1.primary_nick, "who is wuest?") }
+  let(:message_3) { Message::Message.new(user_1.primary_nick, "who is magenta?") }
   let(:handler_1) { Handlers::Bio.new(message: message_1) }
   let(:handler_2) { Handlers::Bio.new(message: message_2) }
+  let(:handler_3) { Handlers::Bio.new(message: message_3) }
 
   before do
    allow(message_1).to receive(:sender)      { user_1 }
@@ -29,6 +31,13 @@ describe "Handlers::Bio" do
       it "calls formatted_bio" do
         expect(user_2).to receive(:formatted_bio)
         handler_2.process
+      end
+    end
+
+    context "no nick specified" do
+      it "calls formatted_bio" do
+        expect(user_1).to_not receive(:formatted_bio)
+        handler_3.process
       end
     end
 
