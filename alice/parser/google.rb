92304be79c4f13cee0c2195a9f3a8f967b4a8df3
CoralineAda <coraline@idolhands.com>
2014-12-14 17:45:15 -0600

    Rescue bad retrievals in Parser::Google

--- alice/parser/google.rb
+++ alice/parser/google.rb
@@ -3,7 +3,11 @@ module Parser
     def self.fetch(topic)
       if results = RubyWebSearch::Google.search(query: topic).results
         content = results[0..2].map do |result|
-          Parser::URL.new(result[:url]).content
+          begin
+            Parser::URL.new(result[:url]).content
+          rescue
+            ""
+          end
         end.flatten.join(' ')
       end
     end
