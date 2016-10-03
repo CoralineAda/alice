f8400528b0ce10166c2794631b782a5d7863f804
Coraline Ada Ehmke <coraline@idolhands.com>
2016-10-03 14:22:09 -0500

    Create users

--- alice.rb
+++ alice.rb
@@ -30,7 +30,7 @@ I18n.enforce_available_locales = false
 bot = Slackbotsy::Bot.new(config) do
   hear /(.+)/ do |mdata|
     begin
-      Alice::User.ensure_user(user_name)
+      User.ensure_user(user_name)
       Pipeline::Listener.new.route(user_name, mdata[1])
     rescue
       ""
