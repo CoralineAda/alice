6c41b8e973479351e581747059033684dd7d9be6
Coraline Ada Ehmke <coraline@idolhands.com>
2016-10-08 10:22:54 -0500

    Working on emotes

--- alice/pipeline/mediator.rb
+++ alice/pipeline/mediator.rb
@@ -27,15 +27,17 @@ module Pipeline
     end
 
     def self.reply_with(message)
+      Alice::Util::Logger.info("!!! replying")
       text = Util::Sanitizer.process(message)
       text = Util::Sanitizer.initial_upcase(text)
       text.to_s
     end
 
     def self.emote(message)
+      Alice::Util::Logger.info("!!! emoting")
       text = Util::Sanitizer.process(message)
       text = Util::Sanitizer.initial_downcase(text)
-      "_#{text}_"
+      "/me #{text}"
     end
 
   end
