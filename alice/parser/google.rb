099364aa1405be9156288160d14c8091b527eda3
Coraline Ada Ehmke <coraline@idolhands.com>
2016-11-20 21:12:41 -0600

    Return the most declarative answer from Google

--- alice/parser/google.rb
+++ alice/parser/google.rb
@@ -6,6 +6,8 @@ module Parser
     attr_accessor :question
     attr_reader :answer
 
+    DECLARATIVE_DETECTOR = /\b#{Grammar::LanguageHelper::INFO_VERBS * '|\b'}/ix
+
     def initialize(question)
       @question = question.gsub("+", "plus").gsub(" ", "+")
     end
@@ -18,13 +20,17 @@ module Parser
 
     def results
       doc = Nokogiri::HTML(open("https://www.google.com/search?q=#{question}"))
-      answer = doc.css("div span.st").map(&:text).sort{|a,b| a.length <=> b.length}.last.split.join(' ')
+      answer = doc.css("div span.st").map(&:text).sort{|a,b| declarative_index(a) <=> declarative_index(b)}.first.split.join(' ')
       Alice::Util::Logger.info "*** Parser::Google: Answered \"#{self.question}\" with #{answer}"
       return answer
     rescue Exception => e
       Alice::Util::Logger.info "*** Parser::Google: Unable to process \"#{self.question}\": #{e}"
       Alice::Util::Logger.info e.backtrace
       ""
+    end
+
+    def declarative_index(answer)
+      answer =~ DECLARATIVE_DETECTOR || 1000
     end
 
   end
