a832be17968cfc9d71f293ca97a9024ae724775d
Coraline Ada Ehmke <coraline@idolhands.com>
2017-01-01 19:47:07 -0600

    Don't persist messages for now

--- alice/message/persisted.rb
+++ alice/message/persisted.rb
@@ -1,14 +1,14 @@
-module Message
-  class Persisted
-
-    include Mongoid::Document
-    include Mongoid::Timestamps
-
-    field :message
-    field :context_id
-
-    belongs_to :context
-    store_in collection: "alice_messages"
-
-  end
-end
+# module Message
+#   class Persisted
+#
+#     include Mongoid::Document
+#     include Mongoid::Timestamps
+#
+#     field :message
+#     field :context_id
+#
+#     belongs_to :context
+#     store_in collection: "alice_messages"
+#
+#   end
+# end
