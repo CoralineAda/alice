7a334d38dfa7fadc3f65749a40cba7271b1dcfdf
Coraline Ada Ehmke <coraline@idolhands.com>
2017-06-23 22:45:40 -0500

    And again

--- alice/handlers/bio.rb
+++ alice/handlers/bio.rb
@@ -12,35 +12,23 @@ module Handlers
     private
 
     def handle_bio(quoted, sender)
-      if command && command.predicate && command.predicate != "bio" && command_string.content !~ /^who is/
-        update_bio(command_string.predicate)
-      elsif subject
-        Context.from(subject.primary_nick).current!
-        return_bio(subject)
-      elsif command_string.content[0] == "!"
-        return_bio(sender)
+#      require 'pry'; binding.pry
+      if command_string.verb == "bio"
+        if command_string.predicate.present?
+          message.sender.update_bio(command_string.predicate)
+          message.response = "I've recorded the details in my notebook."
+        else
+          message.response = sender.formatted_bio
+        end
       else
-        message.response = "I don't know anyone by that name."
+        user = ::User.from(command_string.components.join(' '))
+        message.response = user.formatted_bio if user
       end
-    end
-
-    def return_bio(sender)
-      who = subject
-      who ||= sender
-      text = "I don't know who that is." unless who
-      text = who.formatted_bio if who && who.formatted_bio
-      text ||= "I don't seem to know anything about them."
-      message.response = text
-    end
-
-    def subject
-      ::User.from(command_string.components.join(' '))
+      message.response ||= "I don't know anyone by that name."
     end
 
     def update_bio(quoted)
       return unless quoted
-      message.sender.update_bio(quoted)
-      message.response = "I've recorded the details in my notebook."
     end
 
   end
