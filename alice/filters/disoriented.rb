659bde8b6dc7a2c1d93fd922b8e3bfef2b490d91
Coraline Ada Ehmke <coraline@idolhands.com>
2017-10-15 12:46:27 -0500

    Fix return value in disoriented filter

--- alice/filters/disoriented.rb
+++ alice/filters/disoriented.rb
@@ -7,6 +7,7 @@ module Filters
       directions = (Place::DIRECTIONS + ["up", "down", "sideways", "dennis", "diagonally"])
       directions = directions.sort{|a,b| rand(10) <=> rand(10)}
       to_process.gsub!(/east|west|north|south/) { directions.pop }
+      return to_process
     end
 
   end
