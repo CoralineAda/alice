646ef6d5fab1579a04a343b18ec998994a579b12
Coraline Ada Ehmke <coraline@idolhands.com>
2018-08-02 22:59:43 -0700

    Fix issue with greeting

--- alice/handlers/greeting.rb
+++ alice/handlers/greeting.rb
@@ -6,11 +6,7 @@ module Handlers
     include Behavior::HandlesCommands
 
     def greet_sender
-      greetee = subject.primary_nick
-      if command_string.fragment.include?("to")
-        greetee = command_string.fragment.split("to")[1]
-      end
-      message.response = Util::Randomizer.greeting(greetee)
+      message.response = Util::Randomizer.greeting(subject.primary_nick)
     end
 
     private
